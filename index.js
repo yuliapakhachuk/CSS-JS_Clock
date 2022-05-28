372407a430514c0497eca19c2578e9351ff9b122
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2022-05-28 18:56:06 +0200

    fix bug of transition adds styles of hour hand

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const hourHand = document.querySelector(".hour-hand");
 const minHand = document.querySelector(".min-hand");
 const secondHand = document.querySelector(".second-hand");
 
+const clearTransition = (element, degrees) =>
+    element.style.transition = (degrees === 90) ? "none" : null;
+
 function setDate() { 
     const currentTime = new Date();
+
     const hours = currentTime.getHours();
     const minutes = currentTime.getMinutes();
     const seconds = currentTime.getSeconds();
+    
+    const hourDegree = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+    const minuteDegree = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+    const secondDegree = (seconds / 60) * 360 + 90;
 
-    hourHand.style.transform = `rotate(${((hours / 12) * 360) + ((minutes/60)*30) + 90}deg)`;
-    minHand.style.transform = `rotate(${((minutes / 60) * 360) + ((seconds/60)*6) + 90}deg)`;
-    secondHand.style.transform = `rotate(${((seconds / 60) * 360) + 90}deg)`;
+    clearTransition(hourHand, hourDegree);
+    clearTransition(minHand, minuteDegree);
+    clearTransition(secondHand, secondDegree);
+
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
+    minHand.style.transform = `rotate(${minuteDegree}deg)`;
+    secondHand.style.transform = `rotate(${secondDegree}deg)`;
+    
+    setTimeout(() => requestAnimationFrame(setDate), 1000);
 }
 
-setInterval(setDate, 1000);
+
+setDate();
